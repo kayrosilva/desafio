a4199984d1333e1e173620c25fcbc6dc66558adb
Kayro Silva <kayro.g.silva21@gmail.com>
2024-12-19 14:12:14 -0300

    backend

---
+++ src/main/java/com/github/kayrosilva/desafio/model/DTO/ClienteAtualizacaoDTO.java
@@ -0,0 +1,45 @@
+package com.github.kayrosilva.desafio.model.DTO;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import java.time.LocalDate;
+
+public class ClienteAtualizacaoDTO {
+
+    @NotNull
+    @Size(min = 2, max = 100)
+    private String nome;
+
+    @NotNull
+    @Size(min = 2, max = 100)
+    private String sobrenome;
+
+    @NotNull
+    private LocalDate nascimento;
+
+    // Getters e Setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public LocalDate getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(LocalDate nascimento) {
+        this.nascimento = nascimento;
+    }
+}
+
